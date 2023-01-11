32c90648a62c87cd5da330d4c7edff7bab4686cd
xiang-xx <xiang9311@gmail.com>
2023-01-11 17:30:58 +0800

    feat: get dola user id

--- contract.go
+++ contract.go
@@ -2,7 +2,6 @@ package gosuilending
 
 import (
 	"context"
-	"errors"
 	"math/big"
 
 	"github.com/coming-chat/go-sui/client"
@@ -86,6 +85,7 @@ type ContractConfig struct {
 	PriceOracle                string
 	Storage                    string
 	WormholeState              string
+	UserManagerInfo            string
 }
 
 type Contract struct {
@@ -98,6 +98,7 @@ type Contract struct {
 	priceOracle                *types.HexData
 	storage                    *types.HexData
 	wormholeState              *types.HexData
+	userManagerInfo            *types.HexData
 }
 
 func NewContract(client *client.Client, config ContractConfig) (*Contract, error) {
@@ -122,6 +123,9 @@ func NewContract(client *client.Client, config ContractConfig) (*Contract, error
 		return nil, err
 	}
 	if contract.wormholeState, err = types.NewHexData(config.WormholeState); err != nil {
+		return nil, err
+	}
+	if contract.userManagerInfo, err = types.NewHexData(config.UserManagerInfo); err != nil {
 		return nil, err
 	}
 	return contract, nil
@@ -183,277 +187,4 @@ func (c *Contract) Repay(ctx context.Context, signer types.Address, typeArgs []s
 	}
 	resp, err := c.client.MoveCall(ctx, signer, *c.lendingPortalPackageId, "lending", "repay", typeArgs, args, callOptions.Gas, callOptions.GasBudget)
 	return resp, err
-}
-
-func (c *Contract) GetDolaTokenLiquidity(ctx context.Context, signer types.Address, dolaPoolId uint16, callOptions CallOptions) (liquidity *big.Int, err error) {
-	args := []any{
-		*c.poolManagerInfo,
-		dolaPoolId,
-	}
-
-	tx, err := c.client.MoveCall(ctx, signer, *c.externalInterfacePackageId, "interfaces", "get_dola_token_liquidity", []string{}, args, callOptions.Gas, callOptions.GasBudget)
-	if err != nil {
-		return
-	}
-
-	effects, err := c.client.DryRunTransaction(ctx, tx)
-	if err != nil {
-		return
-	}
-
-	err = parseLastEvent(effects, func(event types.Event) error {
-		tokenLiquidity := event.(map[string]interface{})["moveEvent"].(map[string]interface{})["fields"].(map[string]interface{})["token_liquidity"].(string)
-		liquidity, _ = big.NewInt(0).SetString(tokenLiquidity, 10)
-		return nil
-	})
-	return
-}
-
-func (c *Contract) GetAppTokenLiquidity(ctx context.Context, signer types.Address, appId uint16, dolaPoolId uint16, callOptions CallOptions) (liquidity *big.Int, err error) {
-	args := []any{
-		*c.poolManagerInfo,
-		appId,
-		dolaPoolId,
-	}
-	tx, err := c.client.MoveCall(ctx, signer, *c.externalInterfacePackageId, "interfaces", "get_app_token_liquidity", []string{}, args, callOptions.Gas, callOptions.GasBudget)
-	if err != nil {
-		return
-	}
-
-	effects, err := c.client.DryRunTransaction(ctx, tx)
-	if err != nil {
-		return
-	}
-
-	err = parseLastEvent(effects, func(event types.Event) error {
-		tokenLiquidity := event.(map[string]interface{})["moveEvent"].(map[string]interface{})["fields"].(map[string]interface{})["token_liquidity"].(string)
-		var b bool
-		liquidity, b = big.NewInt(0).SetString(tokenLiquidity, 10)
-		if !b {
-			return errors.New("event parse failed: tokenLiquidity is not integer")
-		}
-		return nil
-	})
-	return
-}
-
-func (c *Contract) GetUserTokenDebt(ctx context.Context, signer types.Address, dolaUserId string, dolaPoolId uint16, callOptions CallOptions) (debtAmount *big.Int, debtValue *big.Int, err error) {
-	args := []any{
-		*c.storage,
-		*c.priceOracle,
-		dolaUserId,
-		dolaPoolId,
-	}
-	tx, err := c.client.MoveCall(ctx, signer, *c.externalInterfacePackageId, "interfaces", "get_user_token_debt", []string{}, args, callOptions.Gas, callOptions.GasBudget)
-	if err != nil {
-		return
-	}
-
-	effects, err := c.client.DryRunTransaction(ctx, tx)
-	if err != nil {
-		return
-	}
-
-	err = parseLastEvent(effects, func(event types.Event) error {
-		fields := event.(map[string]interface{})["moveEvent"].(map[string]interface{})["fields"].(map[string]interface{})
-		debtAmountStr := fields["debt_amount"].(string)
-		debtValueStr := fields["debt_value"].(string)
-		var ok bool
-		debtAmount, ok = big.NewInt(0).SetString(debtAmountStr, 10)
-		if !ok {
-			return errors.New("debtAmount parse error")
-		}
-		debtValue, ok = big.NewInt(0).SetString(debtValueStr, 10)
-		if !ok {
-			return errors.New("debtValue parse error")
-		}
-		return nil
-	})
-	return
-}
-
-func (c *Contract) GetUserCollateral(ctx context.Context, signer types.Address, dolaUserId string, dolaPoolId uint16, callOptions CallOptions) (collateralAmount *big.Int, collateralValue *big.Int, err error) {
-	args := []any{
-		*c.storage,
-		*c.priceOracle,
-		dolaUserId,
-		dolaPoolId,
-	}
-	tx, err := c.client.MoveCall(ctx, signer, *c.externalInterfacePackageId, "interfaces", "get_user_collateral", []string{}, args, callOptions.Gas, callOptions.GasBudget)
-	if err != nil {
-		return
-	}
-
-	effects, err := c.client.DryRunTransaction(ctx, tx)
-	if err != nil {
-		return
-	}
-
-	err = parseLastEvent(effects, func(event types.Event) error {
-		fields := event.(map[string]interface{})["moveEvent"].(map[string]interface{})["fields"].(map[string]interface{})
-		collateralAmountStr := fields["collateral_amount"].(string)
-		collateralValueStr := fields["collateral_value"].(string)
-		var ok bool
-		collateralAmount, ok = big.NewInt(0).SetString(collateralAmountStr, 10)
-		if !ok {
-			return errors.New("collateralAmount parse error")
-		}
-		collateralValue, ok = big.NewInt(0).SetString(collateralValueStr, 10)
-		if !ok {
-			return errors.New("collateralValue parse error")
-		}
-		return nil
-	})
-	return
-}
-
-func (c *Contract) GetReserveInfo(ctx context.Context, signer types.Address, dolaPoolId uint16, callOptions CallOptions) (reserveInfo *ReserveInfo, err error) {
-	args := []any{
-		*c.poolManagerInfo,
-		*c.storage,
-		dolaPoolId,
-	}
-	tx, err := c.client.MoveCall(ctx, signer, *c.externalInterfacePackageId, "interfaces", "get_reserve_info", []string{}, args, callOptions.Gas, callOptions.GasBudget)
-	if err != nil {
-		return
-	}
-
-	effects, err := c.client.DryRunTransaction(ctx, tx)
-	if err != nil {
-		return
-	}
-
-	err = parseLastEvent(effects, func(event types.Event) error {
-		reserveInfo = &ReserveInfo{}
-		var b bool
-		fields := event.(map[string]interface{})["moveEvent"].(map[string]interface{})["fields"].(map[string]interface{})
-		reserveInfo.BorrowApy = int(fields["borrow_apy"].(float64))
-		reserveInfo.Debt, b = new(big.Int).SetString(fields["debt"].(string), 10)
-		if !b {
-			return errors.New("parse reserve failed")
-		}
-		reserveInfo.Reserve, b = new(big.Int).SetString(fields["reserve"].(string), 10)
-		if !b {
-			return errors.New("parse reserve failed")
-		}
-		reserveInfo.SupplyApy = int(fields["supply_apy"].(float64))
-		reserveInfo.UtilizationRate = int(fields["utilization_rate"].(float64))
-		reserveInfo.DolaPoolId = uint16(fields["dola_pool_id"].(float64))
-		return nil
-	})
-	return
-}
-
-func (c *Contract) GetUserAllowedBorrow(ctx context.Context, signer types.Address, dolaUserId string, borrowPoolId uint16, callOptions CallOptions) (amount *big.Int, err error) {
-	args := []any{
-		*c.poolManagerInfo,
-		*c.storage,
-		*c.priceOracle,
-		dolaUserId,
-		borrowPoolId,
-	}
-	tx, err := c.client.MoveCall(ctx, signer, *c.externalInterfacePackageId, "interfaces", "get_user_allowed_borrow", []string{}, args, callOptions.Gas, callOptions.GasBudget)
-	if err != nil {
-		return
-	}
-
-	effects, err := c.client.DryRunTransaction(ctx, tx)
-	if err != nil {
-		return
-	}
-
-	err = parseLastEvent(effects, func(event types.Event) error {
-		fields := event.(map[string]interface{})["moveEvent"].(map[string]interface{})["fields"].(map[string]interface{})
-		amount, _ = big.NewInt(0).SetString(fields["borrow_amount"].(string), 10)
-		if amount.Cmp(big.NewInt(0)) == 0 {
-			if fields["reason"] != "" && fields["reason"] != nil {
-				return errors.New(fields["reason"].(string))
-			}
-		}
-		return nil
-	})
-	return
-}
-
-func (c *Contract) GetUserLendingInfo(ctx context.Context, signer types.Address, dolaUserId string, callOptions CallOptions) (userLendingInfo *UserLendingInfo, err error) {
-	args := []any{
-		*c.storage,
-		*c.priceOracle,
-		dolaUserId,
-	}
-	tx, err := c.client.MoveCall(ctx, signer, *c.externalInterfacePackageId, "interfaces", "get_user_lending_info", []string{}, args, callOptions.Gas, callOptions.GasBudget)
-	if err != nil {
-		return
-	}
-
-	effects, err := c.client.DryRunTransaction(ctx, tx)
-	if err != nil {
-		return
-	}
-
-	err = parseLastEvent(effects, func(event types.Event) error {
-		fields := event.(map[string]interface{})["moveEvent"].(map[string]interface{})["fields"].(map[string]interface{})
-		userLendingInfo = &UserLendingInfo{}
-		userLendingInfo.TotalCollateralValue = new(big.Int).SetUint64(uint64(fields["total_collateral_value"].(float64)))
-		userLendingInfo.TotalDebtValue = new(big.Int).SetUint64(uint64(fields["total_debt_value"].(float64)))
-
-		if fields["collateral_infos"] != "" {
-			infos := fields["collateral_infos"].([]interface{})
-			userLendingInfo.CollateralInfos = make([]CollateralItem, 0, len(infos))
-			for _, info := range infos {
-				infoMap := info.(map[string]interface{})
-				innerFields := infoMap["fields"].(map[string]interface{})
-				dolaPoolId := uint16(innerFields["dola_pool_id"].(float64))
-				amount, _ := new(big.Int).SetString(innerFields["collateral_amount"].(string), 10)
-				value, _ := new(big.Int).SetString(innerFields["collateral_value"].(string), 10)
-
-				userLendingInfo.CollateralInfos = append(userLendingInfo.CollateralInfos, CollateralItem{
-					Type:             infoMap["type"].(string),
-					CollateralAmount: amount,
-					CollateralValue:  value,
-					DolaPoolId:       dolaPoolId,
-				})
-			}
-		}
-
-		if fields["debt_infos"] != "" {
-			infos := fields["debt_infos"].([]interface{})
-			userLendingInfo.DebtInfos = make([]DebtItem, 0, len(infos))
-			for _, info := range infos {
-				infoMap := info.(map[string]interface{})
-				innerFields := infoMap["fields"].(map[string]interface{})
-				dolaPoolId := uint16(innerFields["dola_pool_id"].(float64))
-				amount, _ := new(big.Int).SetString(innerFields["debt_amount"].(string), 10)
-				value, _ := new(big.Int).SetString(innerFields["debt_value"].(string), 10)
-
-				userLendingInfo.DebtInfos = append(userLendingInfo.DebtInfos, DebtItem{
-					Type:       infoMap["type"].(string),
-					DebtAmount: amount,
-					DebtValue:  value,
-					DolaPoolId: dolaPoolId,
-				})
-			}
-		}
-
-		return nil
-	})
-	return
-}
-
-func parseLastEvent(effects *types.TransactionEffects, f func(event types.Event) error) (err error) {
-	if effects.Status.Status != "success" {
-		return errors.New(effects.Status.Error)
-	}
-
-	if len(effects.Events) == 0 {
-		return errors.New("invalid events")
-	}
-
-	defer func() {
-		if merr := recover(); merr != nil {
-			err = errors.New("event parse failed")
-		}
-	}()
-
-	return f(effects.Events[len(effects.Events)-1])
 }
--- contract_query_test.go
+++ contract_query_test.go
@@ -24,15 +24,34 @@ func AssertNil(err error) {
 	}
 }
 
-func TestContract_GetDolaTokenLiquidity(t *testing.T) {
+func getUserManagerInfo() *types.HexData {
+	t, err := types.NewHexData("0x489f3e671c6d493d6b28aa1c682f71ce80c8e263")
+	AssertNil(err)
+	return t
+}
+
+func getExternalInterfacePackageId() *types.HexData {
 	externalInterfacePackageId, err := types.NewHexData("0x20c2b9cb6d88de7dcf2b6ba98900058e1d58781c")
 	AssertNil(err)
+	return externalInterfacePackageId
+}
+
+func getPoolManager() *types.HexData {
 	poolManager, err := types.NewHexData("0x6f68637e8f8f98ac62d7a08efaeacebbcd620ce9")
 	AssertNil(err)
+	return poolManager
+}
+
+func getTestAddressAndGas() (*types.Address, *types.HexData) {
 	address, err := types.NewAddressFromHex("0x4c62953a63373c9cbbbd04a971b9f72109cf9ef3")
 	AssertNil(err)
 	gasObj, err := types.NewHexData("0x085c11efc7d4d405f75c91f8a1990de89182acd5")
 	AssertNil(err)
+	return address, gasObj
+}
+
+func TestContract_GetDolaTokenLiquidity(t *testing.T) {
+	address, gasObj := getTestAddressAndGas()
 	type fields struct {
 		client                     *client.Client
 		poolManagerInfo            *types.HexData
@@ -54,8 +73,8 @@ func TestContract_GetDolaTokenLiquidity(t *testing.T) {
 			name: "case",
 			fields: fields{
 				client:                     getDevClient(),
-				poolManagerInfo:            poolManager,
-				externalInterfacePackageId: externalInterfacePackageId,
+				poolManagerInfo:            getPoolManager(),
+				externalInterfacePackageId: getExternalInterfacePackageId(),
 			},
 			args: args{
 				ctx:         context.Background(),
@@ -83,14 +102,7 @@ func TestContract_GetDolaTokenLiquidity(t *testing.T) {
 }
 
 func TestContract_GetAppTokenLiquidity(t *testing.T) {
-	externalInterfacePackageId, err := types.NewHexData("0x20c2b9cb6d88de7dcf2b6ba98900058e1d58781c")
-	AssertNil(err)
-	poolManager, err := types.NewHexData("0x6f68637e8f8f98ac62d7a08efaeacebbcd620ce9")
-	AssertNil(err)
-	address, err := types.NewAddressFromHex("0x4c62953a63373c9cbbbd04a971b9f72109cf9ef3")
-	AssertNil(err)
-	gasObj, err := types.NewHexData("0x085c11efc7d4d405f75c91f8a1990de89182acd5")
-	AssertNil(err)
+	address, gasObj := getTestAddressAndGas()
 	type fields struct {
 		client                     *client.Client
 		externalInterfacePackageId *types.HexData
@@ -113,8 +125,8 @@ func TestContract_GetAppTokenLiquidity(t *testing.T) {
 			name: "case",
 			fields: fields{
 				client:                     getDevClient(),
-				poolManagerInfo:            poolManager,
-				externalInterfacePackageId: externalInterfacePackageId,
+				poolManagerInfo:            getPoolManager(),
+				externalInterfacePackageId: getExternalInterfacePackageId(),
 			},
 			args: args{
 				ctx:         context.Background(),
@@ -136,6 +148,59 @@ func TestContract_GetAppTokenLiquidity(t *testing.T) {
 			_, err := c.GetAppTokenLiquidity(tt.args.ctx, tt.args.signer, tt.args.appId, tt.args.dolaPoolId, tt.args.callOptions)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Contract.GetAppTokenLiquidity() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestContract_GetDolaUserId(t *testing.T) {
+	address, gasObj := getTestAddressAndGas()
+	type fields struct {
+		client                     *client.Client
+		externalInterfacePackageId *types.HexData
+		userManagerInfo            *types.HexData
+	}
+	type args struct {
+		ctx         context.Context
+		signer      types.Address
+		dolaChainId uint16
+		user        string
+		callOptions CallOptions
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "case",
+			fields: fields{
+				client:                     getDevClient(),
+				externalInterfacePackageId: getExternalInterfacePackageId(),
+				userManagerInfo:            getUserManagerInfo(),
+			},
+			args: args{
+				ctx:         context.Background(),
+				signer:      *address,
+				dolaChainId: 0,
+				user:        address.String(),
+				callOptions: CallOptions{Gas: gasObj, GasBudget: 10000},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Contract{
+				client:                     tt.fields.client,
+				externalInterfacePackageId: tt.fields.externalInterfacePackageId,
+				userManagerInfo:            tt.fields.userManagerInfo,
+			}
+			_, err := c.GetDolaUserId(tt.args.ctx, tt.args.signer, tt.args.dolaChainId, tt.args.user, tt.args.callOptions)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Contract.GetDolaUserId() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 		})
